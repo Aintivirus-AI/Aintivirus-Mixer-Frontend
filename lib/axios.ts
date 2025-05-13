9d79ce1b6d6f3dff310f5df39c8297bf07abb812
goldliondevx <eityeily@gmail.com>
2025-05-12 20:52:41 -0400

    update config for build

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// export default axios.create({
-//   baseURL: 'https://api.mix.aintivirus.ai/v1',
-// });
-
 export default axios.create({
-    baseURL: "http://localhost:8000/v1"
-})
+  baseURL: 'https://api.mix.aintivirus.ai/v1',
+});
+
+// export default axios.create({
+//     baseURL: "http://localhost:8000/v1"
+// })
